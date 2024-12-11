713754fcd851b5d170114ddab89c05fb462ba071
DimiPaparas <dp1u20@soton.ac.uk>
2024-12-11 10:11:50 +0000

    Add config-enabled firmware.

--- leaf_sensor/leaf_sensor.ino
+++ leaf_sensor/leaf_sensor.ino
@@ -1,9 +1,11 @@
 #include <LMP91000.h>
 #include <Wire.h>
 #include <WiFiNINA.h>  // WiFi library for the Arduino Nano 33 IoT
+#include <ArduinoJson.h>
  
 LMP91000 pStat[4];
 const int MENB[4] = {2, 3, 4, 5};
+const int ANALOG[4] = {A3, A2, A1, A0};
 
 const int16_t opVolt = 3300; //milliVolts if working with a 3.3V device
 const uint8_t resolution = 10; //10-bits
@@ -15,12 +17,26 @@ IPAddress local_IP(192, 168, 1, 100);
 IPAddress gateway(192, 168, 1, 1);
 IPAddress subnet(255, 255, 255, 0);
 
-WiFiServer server(123);
+WiFiServer server(80);
 
 int sensorValue[4];
 const int biasValue = -7;
 
 int status = WL_IDLE_STATUS;
+
+// 1V = 1000 and 24% of 3.3V is 792
+// anything higher will make the chip
+// output zero
+
+uint16_t sample_interval = 10;
+uint8_t range = 6;
+int16_t gain[4] = {2, 2, 2, 2};
+int16_t pre_stepV[4] = {0, 0, 0, 0};
+int16_t v1[4] = {0, 0, 0, 0};
+int16_t v2[4] = {0, 0, 0, 0};
+uint32_t quietTime = 1000;
+uint32_t t1 = 1000;
+uint32_t t2 = 1000;
 
 void setup() {
   Wire.begin();
@@ -28,14 +44,14 @@ void setup() {
 
   // Initialize all MENB pins and potentiostats in bulk
   for (int i = 0; i < 4; i++) {
-    pinMode(MENB[i], OUTPUT);
-    digitalWrite(MENB[i], LOW);
-    initPotentiostat(pStat[i], 2);
+    pStat[i].setMENB(MENB[i]);
+    pStat[i].enable();
+    initPotentiostat(pStat[i], gain[i]);
+    pStat[i].disable();
   }
-  
-  delay(2000);
 
   startAccessPoint();
+  delay(1000);
   server.begin();
   Serial.println("TCP server started.");
 }
@@ -59,21 +75,20 @@ void loop() {
     while (client.connected()) {
       // TODO: Somehow add the runAmp function here...
       // readAndSendData(client);
-      client.print("400");
-      int16_t pre_stepV[] = {0,0,0,0};
-      // 1V = 1000 and 24% of 3.3V is 792
-      // anything higher will make the chip
-      // output zero
-      int16_t v1[] = {100,200,400,700};
-      int16_t v2[] = {0,0,0,0};
-      runAmp(
-        10, 6,
-        pre_stepV, v1, v2,
-        1000, 1000, 1000,
-        client
-      );
+
+      String jsonString = client.readStringUntil('\n');
+      Serial.println("Received: " + jsonString);
+
+      if (parseJson(jsonString)) {
+        // Run the function with updated values
+        runAmp(sample_interval, range, pre_stepV, v1, v2, quietTime, t1, t2, client);
+        client.print("OK\n");
+      } else {
+        Serial.println("Error parsing JSON.");
+        client.print("STOP\n");
+      }
       client.flush();
-      delay(500);
+      // delay(500);
       if (client.status() != ESTABLISHED) break;
     }
     client.stop();
@@ -86,7 +101,8 @@ void startAccessPoint() {
     Serial.println("WiFi module failed.");
     while (true);
   }
-  WiFi.config(local_IP, gateway, subnet);
+  // WiFi.config(local_IP, gateway, subnet);
+  WiFi.config(local_IP);
   if (WiFi.beginAP(ssid, pass) != WL_AP_LISTENING) {
     Serial.println("AP setup failed.");
     while (true);
@@ -125,7 +141,7 @@ void initPotentiostat(LMP91000 &pStat, uint8_t user_gain) {
 //range = 6 is microamperes
 //range = 3 is milliamperes
 void runAmp(
-  uint16_t samples, uint8_t range,
+  uint16_t sample_interval, uint8_t range,
   int16_t* pre_stepV, int16_t* v1, int16_t* v2,
   uint32_t quietTime, uint32_t t1, uint32_t t2,
   WiFiClient &client
@@ -146,26 +162,38 @@ void runAmp(
 
     // Populate the 2D array
   for (uint8_t j = 0; j < 4; j++) {
-    voltageArray[j][0] = pre_stepV[j];
-    voltageArray[j][1] = v1[j];
-    voltageArray[j][2] = v2[j];
+    voltageArray[j][0] = determineLMP91000Bias(pre_stepV[j]);
+    voltageArray[j][1] = determineLMP91000Bias(v1[j]);
+    voltageArray[j][2] = determineLMP91000Bias(v2[j]);
+    pStat[j].enable();
+    pStat[j].setGain(gain[j]);
+    pStat[j].disable();
   }
 
   //i = 0 is pre-step voltage
   //i = 1 is first step potential
   //i = 2 is second step potential
+  unsigned long startTime;
+  unsigned long refTime = millis();;
   for(uint8_t i = 0; i < 3; i++)
   {
-    uint32_t fs = timeArray[i]/samples;
+    // uint32_t fs = timeArray[i]/sample_interval;
+    uint32_t fs = sample_interval;
+    Serial.println(fs);
 
     for(uint8_t j = 0; j < 4; j++){  
+      pStat[j].enable();
       if(voltageArray[j][i] < 0) pStat[j].setNegBias();
       else pStat[j].setPosBias();
     
       pStat[j].setBias(abs(voltageArray[j][i]));
+      pStat[j].disable();
     }
 
-    unsigned long startTime = millis();
+    if(i==0){
+      startTime = millis();
+    }
+    unsigned long sampleTime = startTime;
     while(millis() - startTime < timeArray[i])
     {
       String data = "";
@@ -175,25 +203,34 @@ void runAmp(
         // Serial.print(millis());
         // Serial.print(",");
         // Serial.println(pow(10,range)*pStat[j].getCurrent(pStat[j].getOutput(A0), opVolt/1000.0, resolution));
-
+        pStat[j].enable();
         data +=
-          String((uint16_t)(opVolt * TIA_BIAS[abs(voltageArray[j][i])] * (voltageArray[j][i] / abs(voltageArray[j][i])))) + ","
-          + String(millis()) + ","
-          + String(pow(10, range) * pStat[j].getCurrent(pStat[j].getOutput(A0), opVolt / 1000.0, resolution)) + ",";
-
+          String((int16_t)(opVolt * TIA_BIAS[abs(voltageArray[j][i])] * (voltageArray[j][i] / abs(voltageArray[j][i])))) + ","
+          + String(millis()-refTime) + ","
+          + String(pow(10, range) * pStat[j].getCurrent(pStat[j].getOutput(ANALOG[j]), opVolt / 1000.0, resolution)) + ",";
+        pStat[j].disable();
         // Send the data over serial and WiFi
         // Serial.print(data);
       }
       data += "\n";
       client.print(data);
       // Serial.print(data);
-      delay(fs);
+      // delay(fs);
+
+      if(sampleTime + fs > startTime + timeArray[i])
+        break;
+      
+      while(sampleTime + fs > millis());
+      sampleTime += fs;
     }
+    startTime += timeArray[i];
   }
 
   //End at 0V
   for(uint8_t j = 0; j < 4; j++){
+    pStat[j].enable();
     pStat[j].setBias(0);
+    pStat[j].disable();
   }
 }
 
@@ -224,4 +261,39 @@ signed char determineLMP91000Bias(int16_t voltage)
     }
   }
   return 0;
+}
+
+bool parseJson(String jsonString) {
+  DynamicJsonDocument doc(512);
+  DeserializationError error = deserializeJson(doc, jsonString);
+
+  if (error) {
+    Serial.println("JSON parsing error: " + String(error.c_str()));
+    return false;
+  }
+
+  // Update parameters only if provided
+  if (doc.containsKey("sample_interval")) sample_interval = doc["sample_interval"];
+  if (doc.containsKey("range")) range = doc["range"];
+  if (doc.containsKey("pre_stepV")) {
+    JsonArray arr = doc["pre_stepV"];
+    for (size_t i = 0; i < 4; i++) pre_stepV[i] = arr[i];
+  }
+  if (doc.containsKey("v1")) {
+    JsonArray arr = doc["v1"];
+    for (size_t i = 0; i < 4; i++) v1[i] = arr[i];
+  }
+  if (doc.containsKey("v2")) {
+    JsonArray arr = doc["v2"];
+    for (size_t i = 0; i < 4; i++) v2[i] = arr[i];
+  }
+  if (doc.containsKey("gain")) {
+    JsonArray arr = doc["gain"];
+    for (size_t i = 0; i < 4; i++) gain[i] = arr[i];
+  }
+  if (doc.containsKey("quietTime")) quietTime = doc["quietTime"];
+  if (doc.containsKey("t1")) t1 = doc["t1"];
+  if (doc.containsKey("t2")) t2 = doc["t2"];
+
+  return true;
 }
